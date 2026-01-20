f08e19858f5192d1ed46ed4a398104b70b71621f
BosEriko <bos@eriko.ph>
2026-01-20 14:54:33 +0800

    feature add request to weatherstack

--- app/controllers/v1/weather_controller.rb
+++ app/controllers/v1/weather_controller.rb
@@ -1,7 +1,18 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class V1::WeatherController < ApplicationController
   def show
     city = params[:city]
-    puts city
-    render json: { wind_speed: 20, tempreture_degrees: 29 }
+    uri = URI("http://api.weatherstack.com/current?access_key=#{ENV['WEATHERSTACK_KEY']}&query=#{city}")
+    response = Net::HTTP.get(uri)
+
+    weather_data = JSON.parse(response)
+
+    render json: {
+      wind_speed: weather_data['current']['wind_speed'],
+      temperature_degrees: weather_data['current']['temperature']
+    }
   end
 end
