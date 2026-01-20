858717a5ec9649a0a98be762d9a9ff4e8b3ef52f
BosEriko <bos@eriko.ph>
2026-01-20 15:05:54 +0800

    add cache

--- app/controllers/v1/weather_controller.rb
+++ app/controllers/v1/weather_controller.rb
@@ -3,16 +3,29 @@ require 'uri'
 require 'json'
 
 class V1::WeatherController < ApplicationController
+  CACHE_TTL = 3.minutes
+
   def show
-    city = params[:city]
-    uri = URI("http://api.weatherstack.com/current?access_key=#{ENV['WEATHERSTACK_KEY']}&query=#{city}")
-    response = Net::HTTP.get(uri)
+    city = params[:city].to_s.downcase
 
-    weather_data = JSON.parse(response)
+    weather = Rails.cache.fetch(weatherstack: "#{city}", expires_in: CACHE_TTL) do
+      fetch_weather_from_weatherstack(city)
+    end
 
-    render json: {
-      wind_speed: weather_data['current']['wind_speed'],
-      temperature_degrees: weather_data['current']['temperature']
-    }
+    render json: weather
   end
+
+  private
+
+    def fetch_weather_from_weatherstack(city)
+      uri = URI("http://api.weatherstack.com/current?access_key=#{ENV['WEATHERSTACK_KEY']}&query=#{city}")
+
+      response = Net::HTTP.get(uri)
+      weather_data = JSON.parse(response)
+
+      {
+        wind_speed: weather_data.dig('current', 'wind_speed'),
+        temperature_degrees: weather_data.dig('current', 'temperature')
+      }
+    end
 end
