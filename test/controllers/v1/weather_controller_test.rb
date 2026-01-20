a948f42823e21a1362a8e605cbb3f6be7f33dc98
BosEriko <bos@eriko.ph>
2026-01-20 16:09:37 +0800

    create simple test for simple return

--- test/controllers/v1/weather_controller_test.rb
+++ test/controllers/v1/weather_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class V1::WeatherControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return JSON weather data" do
+    city = "Melbourne"
+    country = "AU"
+
+    get v1_weather_url(city: city, country: country)
+    assert_response :success
+    assert_equal "application/json; charset=utf-8", @response.content_type
+    json = JSON.parse(@response.body)
+    assert json.key?("wind_speed")
+    assert json.key?("temperature_degrees")
+  end
 end
