ec59aaea4b3908a045dda287194b1ed698c04659
BosEriko <bos@eriko.ph>
2026-01-20 15:20:19 +0800

    add openweather as fallback

--- app/controllers/v1/weather_controller.rb
+++ app/controllers/v1/weather_controller.rb
@@ -8,8 +8,12 @@ class V1::WeatherController < ApplicationController
   def show
     city = params[:city].to_s.downcase
 
-    weather = Rails.cache.fetch(weatherstack: "#{city}", expires_in: CACHE_TTL) do
-      fetch_weather_from_weatherstack(city)
+    weather = Rails.cache.fetch("weather:#{city}", expires_in: CACHE_TTL) do
+      begin
+        fetch_weather_from_weatherstack(city)
+      rescue
+        fetch_weather_from_openweathermap(city)
+      end
     end
 
     render json: weather
@@ -21,11 +25,25 @@ class V1::WeatherController < ApplicationController
       uri = URI("http://api.weatherstack.com/current?access_key=#{ENV['WEATHERSTACK_KEY']}&query=#{city}")
 
       response = Net::HTTP.get(uri)
-      weather_data = JSON.parse(response)
+      data = JSON.parse(response)
+
+      raise "Weatherstack error" if data['error']
 
       {
-        wind_speed: weather_data.dig('current', 'wind_speed'),
-        temperature_degrees: weather_data.dig('current', 'temperature')
+        wind_speed: data.dig('current', 'wind_speed'),
+        temperature_degrees: data.dig('current', 'temperature')
+      }
+    end
+
+    def fetch_weather_from_openweathermap(city)
+      uri = URI("https://api.openweathermap.org/data/2.5/weather?q=#{city}&units=metric&appid=2326504fb9b100bee21400190e4dbe6d")
+
+      response = Net::HTTP.get(uri)
+      data = JSON.parse(response)
+
+      {
+        wind_speed: data.dig('wind', 'speed'),
+        temperature_degrees: data.dig('main', 'temp')
       }
     end
 end
